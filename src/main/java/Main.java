39814026e09db886e35daa88329b29b915e1f519
Puru <purushottambanerjee@gmail.com>
2024-06-23 22:45:36 +0530

    accepted the URL variabe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,7 +30,7 @@ public class Main {
             String path[] = HttpReq[1].split("/",0);
             System.out.println(path[1]);
             if(path[1].equals("echo")) {
-                String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n"+path[2];
+                String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\n"+path[2];
                 clientSocket.getOutputStream().write(resposne.getBytes());
             }
             else{
