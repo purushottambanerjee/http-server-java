8473142c67269dc2d504fff348ad4996cb407baf
Puru <purushottambanerjee@gmail.com>
2024-06-24 20:13:32 +0530

    added multiple request functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,49 +15,49 @@ public class Main {
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
 
-        try {
+     try {
             serverSocket = new ServerSocket(4221);
             serverSocket.setReuseAddress(true);
-            clientSocket = serverSocket.accept(); // Wait for connection from client.
-            //client side conversion of bytes into data.
-            BufferedReader  clientIn = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream()));
+            while (true) {
+                clientSocket = serverSocket.accept(); // Wait for connection from client.
+                //client side conversion of bytes into data.
+                BufferedReader clientIn = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
 
-            //Read the request
-            String req;
-            ArrayList<String> HttpReq=new ArrayList<String>();
+                //Read the request
+                String req;
+                ArrayList<String> HttpReq = new ArrayList<String>();
 
-            //read request completely HTTP requests don't end with EOF but with blank line.
-            while (!(req = clientIn.readLine()).equals(""))
-                HttpReq.add(req);
+                //read request completely HTTP requests don't end with EOF but with blank line.
+                while (!(req = clientIn.readLine()).equals(""))
+                    HttpReq.add(req);
 
-            System.out.println(HttpReq);
-            //Striping URL from the HTTP req
-            String URL[] =HttpReq.get(0).split(" ",0);
-            if(URL[1].equals("/")) {
-                String resposne = "HTTP/1.1 200 OK\r\n\r\n";
-                clientSocket.getOutputStream().write(resposne.getBytes());
+                System.out.println(HttpReq);
+                //Striping URL from the HTTP req
+                String URL[] = HttpReq.get(0).split(" ", 0);
+                if (URL[1].equals("/")) {
+                    String resposne = "HTTP/1.1 200 OK\r\n\r\n";
+                    clientSocket.getOutputStream().write(resposne.getBytes());
 
-            } else if (URL[1].startsWith("/echo/")){
-                String path[] = URL[1].split("/",0);
-                String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:"+path[2].length()+"\r\n\r\n"+path[2];
-                clientSocket.getOutputStream().write(resposne.getBytes());
-            }
-            else if (URL[1].startsWith("/user-agent")){
-                String user_agent[]=new  String[2];
-                for(String s : HttpReq)
-                {
-                    if(s.startsWith("User-Agent"))
-                        user_agent=s.split(": ");
+                } else if (URL[1].startsWith("/echo/")) {
+                    String path[] = URL[1].split("/", 0);
+                    String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + path[2].length() + "\r\n\r\n" + path[2];
+                    clientSocket.getOutputStream().write(resposne.getBytes());
+                } else if (URL[1].startsWith("/user-agent")) {
+                    String user_agent[] = new String[2];
+                    for (String s : HttpReq) {
+                        if (s.startsWith("User-Agent"))
+                            user_agent = s.split(": ");
+                    }
+                    String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + user_agent[1].length() + "\r\n\r\n" + user_agent[1];
+                    clientSocket.getOutputStream().write(resposne.getBytes());
+                } else {
+                    String resposne = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    clientSocket.getOutputStream().write(resposne.getBytes());
                 }
-                String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:"+user_agent[1].length()+"\r\n\r\n"+user_agent[1];
-                clientSocket.getOutputStream().write(resposne.getBytes());
+                clientSocket.close();
+                System.out.println("accepted new connection");
             }
-            else{
-                String resposne = "HTTP/1.1 404 Not Found\r\n\r\n";
-                clientSocket.getOutputStream().write(resposne.getBytes());
-            }
-            System.out.println("accepted new connection");
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
     }
