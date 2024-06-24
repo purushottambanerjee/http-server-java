2938ee6775806c9fe4fc1bcae8f4fc3ab2ff6585
Puru <purushottambanerjee@gmail.com>
2024-06-24 08:56:15 +0530

    added full request reading ability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
@@ -16,8 +17,6 @@ public class Main {
 
         try {
             serverSocket = new ServerSocket(4221);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept(); // Wait for connection from client.
             //client side conversion of bytes into data.
@@ -25,17 +24,29 @@ public class Main {
                     new InputStreamReader(clientSocket.getInputStream()));
 
             //Read the request
-            String req = clientIn.readLine();
-            String HttpReq[] = req.split(" ",0);
-            if(HttpReq[1].equals("/")) {
+            String req;
+            ArrayList<String> HttpReq=new ArrayList<String>();
+            //read request completely HTTP requests don't end with EOF but with blank line.
+            while (!(req = clientIn.readLine()).equals(""))
+                HttpReq.add(req);
+
+            //Striping URL from the HTTP req
+            String URL[] =HttpReq.get(0).split(" ",0);
+            if(URL[1].equals("/")) {
                 String resposne = "HTTP/1.1 200 OK\r\n\r\n";
                 clientSocket.getOutputStream().write(resposne.getBytes());
 
-            } else if (HttpReq[1].startsWith("/echo/")){
-                String path[] = HttpReq[1].split("/",0);
+            } else if (URL[1].startsWith("/echo/")){
+                String path[] = URL[1].split("/",0);
                 String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:"+path[2].length()+"\r\n\r\n"+path[2];
                 clientSocket.getOutputStream().write(resposne.getBytes());
-            } else{
+            }
+            else if (URL[1].startsWith("/user-agent")){
+                String user_agent[] = HttpReq.get(3).split(": ",0);
+                String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:"+user_agent[1].length()+"\r\n\r\n"+user_agent[1];
+                clientSocket.getOutputStream().write(resposne.getBytes());
+            }
+            else{
                 String resposne = "HTTP/1.1 404 Not Found\r\n\r\n";
                 clientSocket.getOutputStream().write(resposne.getBytes());
             }
