a255432963dc331983544c702a8593d5e2046ee5
Puru <purushottambanerjee@gmail.com>
2024-06-25 20:57:03 +0530

    arranged code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,9 @@ public class Main {
       ServerSocket serverSocket = null;
       Socket clientSocket = null;
       String directory="";
+      String Echo= "HTTP/1.1 200 OK\r\n\r\n";
+      String NotFound = "HTTP/1.1 404 Not Found\r\n\r\n";
+      String Respond200 = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:";
       if(args.length>1 && args[0].equals("--directory")){
           directory=args[1];
           System.out.println(args[1]);
@@ -40,56 +43,62 @@ public class Main {
               System.out.println(HttpReq);
               //Striping URL from the HTTP req
               String URL[] = HttpReq.get(0).split(" ", 0);
+              System.out.println(URL[1]);
               if (URL[0].equals("POST")) {
                   StringBuffer data=new StringBuffer();
                   while(clientIn.ready()){
                      data.append((char)clientIn.read());
                  }
-                  String body = data.toString();//.split("\r\n")[3];
-                  //System.out.println(body);
+                  String body = data.toString();
                   Path path = Paths.get(directory,URL[1].split("/")[2]);
                   Files.write(path,body.getBytes());
                   writer.write("HTTP/1.1 201 Created\r\n\r\n".getBytes(StandardCharsets.UTF_8));
               }
               else
               {
-                  if (URL[1].equals("/")) {
-                      String resposne = "HTTP/1.1 200 OK\r\n\r\n";
-                      writer.write(resposne.getBytes(StandardCharsets.UTF_8));
+                  String URL_FULL[] = URL[1].split("/");
+                  String  url_prfix;
+                  if(URL_FULL.length==0)
+                      url_prfix= "";
+                  else
+                      url_prfix=URL_FULL[1];
+                  switch (url_prfix){
+                      case "":
+                          writer.write(Echo.getBytes(StandardCharsets.UTF_8));
+                          break;
+                      case "echo":
+                          String path[] = URL[1].split("/", 0);
+                          Respond200=Respond200 + path[2].length() + "\r\n\r\n" + path[2];
+                          writer.write(Respond200.getBytes());
+                          break;
+                      case "files":
+                          String filename = URL[1].split("/", 0)[2];
+                          File file = new File(directory, filename);
+                          if (file.exists()) {
+                              //reading byte content
+                              byte[] fileContent = Files.readAllBytes(file.toPath());
+                              String httpResponse =
+                                      "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " +
+                                              fileContent.length + "\r\n\r\n" + new String(fileContent);
+                              writer.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+                          } else {
 
-                  } else if (URL[1].startsWith("/echo/")) {
-                      String path[] = URL[1].split("/", 0);
-                      String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + path[2].length() + "\r\n\r\n" + path[2];
-                      writer.write(resposne.getBytes());
-                  } else if (URL[1].startsWith("/user-agent")) {
-                      String user_agent[] = new String[2];
-                      for (String s : HttpReq) {
-                          if (s.startsWith("User-Agent"))
-                              user_agent = s.split(": ");
-                      }
-                      String resposne = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + user_agent[1].length() + "\r\n\r\n" + user_agent[1];
-                      writer.write(resposne.getBytes());
-                  } else if (URL[1].startsWith("/files")) {
-                      String filename = URL[1].split("/", 0)[2];
-                      File file = new File(directory, filename);
-                      System.out.println(file.toPath());
-                      if (file.exists()) {
-                          //reading byte content
-                          byte[] fileContent = Files.readAllBytes(file.toPath());
-                          String httpResponse =
-                                  "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " +
-                                          fileContent.length + "\r\n\r\n" + new String(fileContent);
-                          writer.write(httpResponse.getBytes(StandardCharsets.UTF_8));
-                      } else {
-                          String resposne = "HTTP/1.1 404 Not Found\r\n\r\n";
-                          writer.write(resposne.getBytes());
-                      }
-                  } else {
-                      String resposne = "HTTP/1.1 404 Not Found\r\n\r\n";
-                      writer.write(resposne.getBytes());
+                              writer.write(NotFound.getBytes());
+                          }
+                          break;
+                      case "user-agent":
+                          String user_agent[] = new String[2];
+                          for (String s : HttpReq) {
+                              if (s.startsWith("User-Agent"))
+                                  user_agent = s.split(": ");
+                          }
+                          Respond200+= user_agent[1].length() + "\r\n\r\n" + user_agent[1];
+                          writer.write(Respond200.getBytes());
+                          break;
+                      default:
+                          writer.write(NotFound.getBytes());
                   }
                   clientSocket.close();
-                  System.out.println("accepted new connection");
               }
           }
       } catch(IOException e){
